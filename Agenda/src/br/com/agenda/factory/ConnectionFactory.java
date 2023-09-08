95365e1475e6161591340e1e2c099a7b3f848882
Jeanziin <jjeanfreitass@gmail.com>
2023-09-08 20:35:44 -0300

    Project Created

---
+++ Agenda/src/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,28 @@
+package br.com.agenda.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final String USUARIO = "root";
+    private static final String SENHA = "1234";
+    private static final String URL_DO_BANCO = "jdbc:mysql://localhost:3306/agenda";
+
+    public static Connection createConnectionToMySQl() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL_DO_BANCO, USUARIO, SENHA);
+    }
+
+    public static void closeConnection(Connection conexao) {
+        if (conexao != null) {
+            try {
+                conexao.close();
+                System.out.println("Conexão fechada com sucesso!");
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar a conexão: " + e.getMessage());
+            }
+        }
+    }
+}
